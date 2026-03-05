1c79d87f8f82011b90d204847d527af5bf080d1f
Bianca <157852609+goiabia@users.noreply.github.com>
2026-03-05 00:14:11 -0300

    Update and rename arquivo.py to calculadora.py

---
+++ python/calculadora.py
@@ -0,0 +1,17 @@
+num1 = float(input("Digite o primeiro número: "))
+op = input("Digite a operação (+, -, *, /): ")
+num2 = float(input("Digite o segundo número: "))
+
+if op == "+":
+    resultado = num1 + num2
+elif op == "-":
+    resultado = num1 - num2
+elif op == "*":
+    resultado = num1 * num2
+elif op == "/":
+    resultado = num1 / num2
+else:
+    print("Operação inválida")
+    exit()
+
+print("Resultado:", resultado)
