acff33f8271bea75c5fb1b76c6736114cfa1ca2f
Bianca <157852609+goiabia@users.noreply.github.com>
2026-03-05 00:13:03 -0300

    Update

--- c#/arquivo.cs
+++ c#/arquivo.cs
@@ -1,1 +1,43 @@
+using System;
 
+class Calculadora
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a operação (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = 0;
+
+        switch (op)
+        {
+            case "+":
+                resultado = num1 + num2;
+                break;
+
+            case "-":
+                resultado = num1 - num2;
+                break;
+
+            case "*":
+                resultado = num1 * num2;
+                break;
+
+            case "/":
+                resultado = num1 / num2;
+                break;
+
+            default:
+                Console.WriteLine("Operação inválida");
+                return;
+        }
+
+        Console.WriteLine("Resultado: " + resultado);
+    }
+}
