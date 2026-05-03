5a1527b96d472abb251b40269218ff9b63bfbbcd
Bianca <157852609+goiabia@users.noreply.github.com>
2026-05-02 21:07:28 -0300

    Update ex1.cpp

--- Aula11/C++/ex1.cpp
+++ Aula11/C++/ex1.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <string>
+ 
+class Cachorro{
+ 
+public:
+    // atributos
+    std::string nome;
+    std::string raca;
+    int idade
+ 
+    // construtor: inicializa os atributos do objeto
+    Cachorro(std::string n, std::string r, int i) {
+        nome = n;
+        raca = r;
+        idade = i;
+    }
+ 
+    // método: comportamento do cachorro
+    std::string latir(){
+        return nome + "está latindo: Au Au!"
+    }
+ 
+    // método: comportamento do cachorro
+    std::string comer(std::string comida) {
+        return nome + "está comendo" + comida + ".";
+    }
+};
+ 
+int main() {
+    return 0;
 
+int main() {
+    // Criação de objetos (instâncias da classe Cachorro)
+    Cachorro cachorro1("Rex", "Labrador", 3);
+    Cachorro cachorro2("Bolinha", "Poodle", 5);
+
+    // Acessando atributos dos objetos
+    std::cout << "Nome: " << cachorro1.nome << ", Raça: " << cachorro1.raca 
+              << ", Idade: " << cachorro1.idade << " anos" << std::endl;
+
+    std::cout << "Nome: " << cachorro2.nome << ", Raça: " << cachorro2.raca 
+              << ", Idade: " << cachorro2.idade << " anos" << std::endl;
+
+    // Chamando métodos dos objetos
+    std::cout << cachorro1.latir() << std::endl;
+    std::cout << cachorro2.comer("ração") << std::endl;
+
+    return 0;
+}
+}
